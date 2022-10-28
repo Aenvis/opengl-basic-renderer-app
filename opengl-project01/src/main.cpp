dfc3f4b275768382c7533b4778e5a6e9577986fa
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-10-29 00:16:12 +0200

    rendering window and setting its background color

--- opengl-project01/src/main.cpp
+++ opengl-project01/src/main.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+void ProcessInput(GLFWwindow* window);
+
 int main()
 {
 	glfwInit();
@@ -19,6 +22,7 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -26,5 +30,37 @@ int main()
 		return -1;
 	}
 
+	glViewport(0, 0, 800, 600);
+	GLfloat r = 0.f, g = 0.f, b = 0.f, a=1.f;
+	while (!glfwWindowShouldClose(window))
+	{
+		ProcessInput(window);
+
+		glClearColor(r, g, b, a);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		if (r < 1.f) r += .003f;
+		else r = 0.f;
+		if (g < 1.f) g += .003f;
+		else g = 0.f;
+		if (b < 1.f) b += .003f;
+		else b = 0.f;
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, 1);
 }
