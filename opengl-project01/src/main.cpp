e90b94172a9a29ad0bf40302fc15c219f8410ffa
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-11-02 10:26:16 +0100

    drawing triangle with raw vertices data - just before switching to indexed drawing also simple vertex and fragment shaders

--- opengl-project01/src/main.cpp
+++ opengl-project01/src/main.cpp
@@ -5,6 +5,21 @@
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
 void ProcessInput(GLFWwindow* window);
 
+const char* vertexShaderSource = "#version 460 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 460 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0"
+;
+
 int main()
 {
 	glfwInit();
@@ -12,7 +27,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "openGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "openGL", NULL, NULL);
 
 	if (window == NULL)
 	{
@@ -31,21 +46,104 @@ int main()
 	}
 
 	glViewport(0, 0, 800, 600);
-	GLfloat r = 0.f, g = 0.f, b = 0.f, a=1.f;
+
+	//use only unique vertices and then pass the indices order to draw triangles - indexed drawing
+	float vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	//generate Element Buffer Object (EBO)
+	unsigned int elementBufferObject;
+	glGenBuffers(1, &elementBufferObject);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	//generate Vertex Array Object (VAO)
+	unsigned int vertexArrayObject;
+	glGenVertexArrays(1, &vertexArrayObject);
+	glBindVertexArray(vertexArrayObject);
+	
+	/* DRAW TRIANGLE USING VERTEX BUFFER OBJECT - based on vertices
+	//allocate buffer in gpu's memory, then pass the vertices data as an array
+	unsigned int vertexBufferObject; //VBO
+	glGenBuffers(1, &vertexBufferObject);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+	*/
+
+	//specify how openGL should interpret vertices data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//compile vertex shader
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//check if compilation succeeded
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//compile fragment shader
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//check if compilation succeeded
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//create shader program
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//check if linking succeeded
+	glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	//delete shaders - no longer needed after linking
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//sets the given shaderProgram as active and as a current shader program for drawing
+	glUseProgram(shaderProgram);
+
+	//glBindVertexArray(vertexArrayObject);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+
+	//main loop
 	while (!glfwWindowShouldClose(window))
 	{
 		ProcessInput(window);
-
-		glClearColor(r, g, b, a);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		if (r < 1.f) r += .003f;
-		else r = 0.f;
-		if (g < 1.f) g += .003f;
-		else g = 0.f;
-		if (b < 1.f) b += .003f;
-		else b = 0.f;
-
+		glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+		glDrawElements(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
