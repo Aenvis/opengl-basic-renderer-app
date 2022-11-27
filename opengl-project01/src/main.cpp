2e1fed243d581e9db05033f8a90ba0db7fa372a7
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-11-27 01:53:27 +0100

    Add camera system ; handle keybard input (for movement) and mouse input for looking around and zooming (pitch yaw roll angles)

--- opengl-project01/src/main.cpp
+++ opengl-project01/src/main.cpp
@@ -15,17 +15,35 @@ using namespace renderer;
 
 void Init(unsigned int major, unsigned int minor);
 void ProcessInput(GLFWwindow* window);
+void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+void scrollCallback(GLFWwindow* window, double xpos, double ypos);
 
 //settings
-const unsigned int SCREEN_WIDTH = 800;
-const unsigned int SCREEN_HEIGHT = 600;
+const unsigned int SCREEN_WIDTH = 1280;
+const unsigned int SCREEN_HEIGHT = 720;
+
+glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraForward(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+//mouse input
+float pitch = 0.0f, yaw = -90.0f;
+float lastX = SCREEN_WIDTH / 2;
+float lastY = SCREEN_HEIGHT / 2;
+float fov = 45.0f;
+bool firstMouseInput = true;
 
 int main()
 {
 	Init(4, 6);
 
 	MainWindow window(SCREEN_WIDTH, SCREEN_HEIGHT, "Wojtek");
-
+	glfwSetInputMode(window.Get(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window.Get(), mouseCallback);
+	glfwSetScrollCallback(window.Get(), scrollCallback);
 	Shader myShader("src/shaders/vertex.glsl", "src/shaders/fragment.glsl");
 
 	float vertices[] = {
@@ -94,7 +112,7 @@ int main()
 	vArray.VertexAttribPtr(1, 2, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 
 	//------------
-	//textures - generate and load textures
+	// textures - generate and load textures
 	//------------
 	Texture texture1;
 
@@ -117,20 +135,36 @@ int main()
 	texture2.Load("frame.png", true);
 
 	//------------
-	//maths - local coordinates to screen perspective view
+	// maths - local coordinates to screen perspective view
 	//------------
-	glm::mat4 view = glm::mat4(1.0f);
-	glm::mat4 projection = glm::mat4(1.0f);
+	//glm::mat4 view(1.0f);
 
-	view = glm::translate(view, glm::vec3(-2.0f, 0.0f, -6.0f));
-	projection = glm::perspective(glm::radians(55.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.f);
+	//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
+
+	// zbuffer
 	glEnable(GL_DEPTH_TEST);
+
+	//------------
+	// debug info
+	//------------
+	int minFps = INT_MAX, maxFps = INT_MIN;
+	float startTime = static_cast<float>(glfwGetTime());
 
 	//main loop
 	while (!glfwWindowShouldClose(window.Get()))
 	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		int fps = static_cast<int>(1.0f / deltaTime);
+
+		if (fps < minFps) minFps = fps;
+		else if (fps > maxFps) maxFps = fps;
+
+		std::cout.precision(2);
+		std::cout << fps << " fps :: " << deltaTime << " ms\n";
 		ProcessInput(window.Get());
-		
+
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -144,12 +178,26 @@ int main()
 		myShader.SetInt("objTexture2", 1);
 
 		vArray.Bind();
+
+		//------------
+		// camera and movement
+		//------------
+		glm::vec3 direction{};
+		direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		direction.y = sin(glm::radians(pitch));
+		direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+		cameraForward = glm::normalize(direction);
+
+		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraForward, cameraUp);
+		glm::mat4 projection = glm::perspective(glm::radians(fov), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.f);
+		
 		for (size_t i = 0; i < 10; i++)
 		{
-			glm::mat4 model = glm::mat4(1.0f);
+			glm::mat4 model(1.0f);
 			model = glm::translate(model, cubePositions[i]);
 			model = i%3 ? model : glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
 
+			//mvp stands for model view projection
 			glm::mat4 mvp = projection * view * model;
 			myShader.SetMat4("mvp", mvp);
 			glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -158,6 +206,11 @@ int main()
 		glfwSwapBuffers(window.Get());
 		glfwPollEvents();
 	}
+
+	float endTime = static_cast<float>(glfwGetTime());
+
+	std::cout << "MIN FPS: " << minFps << std::endl << "MAX FPS: " << maxFps << std::endl << "APP WORK TIME: " << endTime - startTime << std::endl;
+
 	return 0;
 }
 
@@ -173,6 +226,52 @@ void ProcessInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, 1);
+
+	const float cameraSpeed = (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ? 4.0f : 2.5f) * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraForward;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraForward;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraForward, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraForward, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+		cameraPos.y -= cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+		cameraPos.y += cameraSpeed;
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouseInput)
+	{
+		lastX = (float)xpos;
+		lastY = (float)ypos;
+		firstMouseInput = false;
+	}
+
+	float xoffset = (float)xpos - lastX;
+	float yoffset = (float)ypos - lastY;
+	lastX = (float)xpos;
+	lastY = (float)ypos;
+
+	const float sensitivity = 0.1f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+	yaw += xoffset;
+	pitch -= yoffset;
+
+	if (pitch > 89.0f) pitch = 89.0f;
+	if (pitch < -89.0f) pitch = -89.0f;
+}
+
+void scrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	const float scrollSpeedMultiplier = 3.0f;
+	fov -= (float)yoffset * scrollSpeedMultiplier;
+	if (fov < 1.0f) fov = 1.0f;
+	if (fov > 55.0f) fov = 55.0f;
 }
 
 
