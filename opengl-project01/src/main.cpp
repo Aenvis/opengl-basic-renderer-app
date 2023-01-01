6a7af572d752b0a464fc65990cb05c7e7c9e236e
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-01-01 13:50:47 +0100

    Nothing changed

--- opengl-project01/src/main.cpp
+++ opengl-project01/src/main.cpp
@@ -45,57 +45,57 @@ int main()
 	Shader cubeShader("src/shaders/vertex.glsl", "src/shaders/fragment.glsl");
 	Shader lampShader("src/shaders/vertexLight.glsl", "src/shaders/fragmentLight.glsl");
 
-	float vertices[] = {
-	-0.5f, -0.5f, -0.5f,
-	 0.5f, -0.5f, -0.5f,
-	 0.5f,  0.5f, -0.5f,
-	 0.5f,  0.5f, -0.5f,
-	-0.5f,  0.5f, -0.5f,
-	-0.5f, -0.5f, -0.5f,
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
 
-	-0.5f, -0.5f,  0.5f,
-	 0.5f, -0.5f,  0.5f,
-	 0.5f,  0.5f,  0.5f,
-	 0.5f,  0.5f,  0.5f,
-	-0.5f,  0.5f,  0.5f,
-	-0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
 
-	-0.5f,  0.5f,  0.5f,
-	-0.5f,  0.5f, -0.5f,
-	-0.5f, -0.5f, -0.5f,
-	-0.5f, -0.5f, -0.5f,
-	-0.5f, -0.5f,  0.5f,
-	-0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
 
-	 0.5f,  0.5f,  0.5f,
-	 0.5f,  0.5f, -0.5f,
-	 0.5f, -0.5f, -0.5f,
-	 0.5f, -0.5f, -0.5f,
-	 0.5f, -0.5f,  0.5f,
-	 0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
 
-	-0.5f, -0.5f, -0.5f,
-	 0.5f, -0.5f, -0.5f,
-	 0.5f, -0.5f,  0.5f,
-	 0.5f, -0.5f,  0.5f,
-	-0.5f, -0.5f,  0.5f,
-	-0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
 
-	-0.5f,  0.5f, -0.5f,
-	 0.5f,  0.5f, -0.5f,
-	 0.5f,  0.5f,  0.5f,
-	 0.5f,  0.5f,  0.5f,
-	-0.5f,  0.5f,  0.5f,
-	-0.5f,  0.5f, -0.5f,
-	};
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+    };
 
 	VertexArray cubeVAO;
 	VertexArray lampVAO;
 	VertexBuffer vBuffer(vertices, sizeof(vertices));
 
-	// position attribute
+    // position attribute
 	cubeVAO.VertexAttribPtr(0, 3, 3 * sizeof(float), (void*)0);
-	
+
 	vBuffer.Bind();
 	lampVAO.VertexAttribPtr(0, 3, 3 * sizeof(float), (void*)0);
 
@@ -110,12 +110,12 @@ int main()
 
 	//main loop
 	while (!glfwWindowShouldClose(window.Get()))
-	{
-		float currentFrame = static_cast<float>(glfwGetTime());
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
+    {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 		int fps = static_cast<int>(1.0f / deltaTime);
-
+		
 		if (fps < minFps) minFps = fps;
 		else if (fps > maxFps) maxFps = fps;
 
@@ -124,15 +124,15 @@ int main()
 
 		ProcessInput(window.Get());
 
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		cubeShader.Use();
 		cubeShader.SetVec3("objectColor", 1.0f, 0.5f, 0.31f); //coral color
 		cubeShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
-		
-		glm::mat4 projection = glm::perspective(glm::radians(camera.GetZoom()), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
-		glm::mat4 view = camera.GetViewMatrix();
+
+        glm::mat4 projection = glm::perspective(glm::radians(camera.GetZoom()), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
 		glm::mat4 model(1.0f);
 		model = glm::translate(model, lightPos);
 		model = glm::scale(model, glm::vec3(1.0f));
@@ -140,13 +140,13 @@ int main()
 		mvp = projection * view * model;
 		cubeShader.Use();
 		cubeShader.SetMat4("mvp", mvp);
-		
+
 		cubeVAO.Bind();
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
 		lampShader.Use();
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, lightPos);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
 		model = glm::scale(model, glm::vec3(0.2f));
 
 		mvp = projection * view * model;
@@ -156,14 +156,14 @@ int main()
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glfwSwapBuffers(window.Get());
-		glfwPollEvents();
-	}
+        glfwPollEvents();
+    }
 
 	float endTime = static_cast<float>(glfwGetTime());
 
 	std::cout << "MIN FPS: " << minFps << std::endl << "MAX FPS: " << maxFps << std::endl << "APP WORK TIME: " << endTime - startTime << std::endl;
 
-	return 0;
+    return 0;
 }
 
 void Init(unsigned int major, unsigned int minor)
